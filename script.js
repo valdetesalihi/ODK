44ff472081aae62eb3c3c70f2158a2d6d11a50f0
unknown <valdete.salihi@student.uni-pr.edu>
2022-03-25 23:47:59 +0100

    Trajnimi 5 ditor...

---
+++ script.js
@@ -0,0 +1,58 @@
+//prompt("Te lutem me shkruaj emrin: ");
+
+/*var emri;
+emri="Zana"
+console.log("Emri im eshte: "+emri); */
+
+/*var emri=prompt("Te lutem sheno emrin: ");
+var mbiemri=prompt("Te lutem sheno mbiemrin: ");
+var mosha=prompt("Sheno moshen: ");
+
+console.log("Emri i plote i perdoruesit eshte: "+emri);
+console.log("Mosha juaj eshte:"+mosha);*/
+
+/*var mosha=prompt("Te lutem me shkruaj moshen: ");
+if(mosha>0){
+    alert("Keni shkruar moshen gabim";)
+}
+else if(mosha=21){
+    alert("Urime ditelindja e 21-te")
+}
+else{
+    alert("Mosha juaj eshte: "+mosha);
+}*/
+
+//Printo numrat cift 10-40 (while loop)
+
+/*var counter=10;
+while(counter<=40){
+    console.log(counter);
+    counter+=2;
+}
+*/
+
+//Printimi i numrave nga 1-10 (for loop)
+/*for(let y = 1 ; y<=10; y++){
+    console.log(y)
+}*/
+
+//Printimi i te gjithe numrave cift nga 10 deri 40(for loop)
+
+/*for (let i = 10; i <= 40; i+=2) {
+  console.log(i);
+} */
+
+//Printoni numrat prej -7 deri ne 20 (for loop)
+
+/*for(let x=-7 ; x<=20; x++){ console.log(x); }*/
+
+//Printoni numrat tek nga numri 5 deri te numri 30 (while loop)
+
+
+
+
+var counter=5;
+while(counter<=30){
+    console.log(counter);
+    counter+=2;
+}
